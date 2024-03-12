9c0cd75cec7802c085468712c5301f8f58c07801
setyaifundip <117325477+setyaifundip@users.noreply.github.com>
2024-03-13 03:09:58 +0700

    Test Update Login.jsx

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -35,7 +35,7 @@ export default function Login({ status, canResetPassword }) {
                 <span className='font-bold pr-1'>
                     Selamat Datang!
                 </span>
-                Silakan masukkan NIM dan Password 
+                Silakan masukkan NPM dan Password 
             </div>
             <form onSubmit={submit}>
                 <div>
