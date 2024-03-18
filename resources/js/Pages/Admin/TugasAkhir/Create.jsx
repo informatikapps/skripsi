ed89f12057f65758e36d0a9d678581351bebeb21
sapril <89457926+saprril@users.noreply.github.com>
2024-03-18 21:28:03 +0700

    Debugging #3 - Why isn't the button working?!?!

--- resources/js/Pages/Admin/TugasAkhir/Create.jsx
+++ resources/js/Pages/Admin/TugasAkhir/Create.jsx
@@ -307,6 +307,7 @@ export default function CreateTA({ auth, errors, status, image, dosen }) {
                                         <PrimaryButton
                                             type="submit"
                                             className="w-24 justify-start bg-amber-500 hover:bg-amber-600 mt-2 ml-2"
+                                            onClick={() => handleSubmitBulk}
                                         >
                                             Tambah
                                         </PrimaryButton>
